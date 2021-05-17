65b6b160aae83b6c908a0264710a467a6651fe84
SamuelDAlencar <sarl.2001@hotmail.com>
2021-05-17 14:16:16 -0300

    week_4_start

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.alert("Page's visual compromised since the same page is being used in multiple examples, including Bootstrap")
+// window.alert("Page's visual compromised since the same page is being used in multiple examples, including Bootstrap")
